3c3c7895d061f144a7714d14a3997aceaeadad98
NoahRAdler <75507781+NoahRAdler@users.noreply.github.com>
2022-10-21 14:02:23 -0400

    Functionality and Error checking update
    Addition: Check for presence of .txt file in todo folder, if none, asks for a .txt file to be added, then closes.
    Addition: File editing is now implemented  
    Addition: There is now a ShowFileContents method with an argument, doesn't toggle read/write, and is a supporting function for file editing
    Addition: Function, displayStrippedPath, that strips the path and returns only the name of a file with its extension. Used for better readability when displaying text.
     
    Change: Editing window now limits number of windows that can be displayed (default is 1)
    Change: Editing window now asks if the user wants to leave stating that the changes will not be saved
    Change: Editing window now has an apply changes button which uses new writeToFile function
    Change: General State and flow control rework to support new functionality
    Change: The function showTodoFiles that shows the available files now uses the displayStrippedPath function
     
    Fix: Global variables are distinguished better with capitals and underscores

--- TimeManagementApp.py
+++ TimeManagementApp.py
@@ -2,148 +2,230 @@ from tkinter import *
 from tkinter import messagebox
 import glob
 
-# From current level, find all .txt files in the designated folder for app
-global files 
-files = glob.glob('./TodoAndReminders/*.txt')
+# From current level, find all .txt files in the designated folder for app,
+# Global use variables indicated as All capitalized and using _ as seperators
+FILES = glob.glob('./TodoAndReminders/*.txt')
+WRITE_WINDOWS_OPEN = 0
+CURRENT_FILE = ""
 
-# Displays files to chose from with the associated input for choosing
-def showTodoFiles():
+if len(FILES) < 1:
+    messagebox.showerror("No Text Files Error","No text files in TodoAndReminders folder, " 
+                    +  "add at least one then restart the app.")
+else:
 
-    fileDisplayTextArea.config(state = NORMAL) # Change state of Text area to allow write
+    # Displays files to chose from with the associated input for choosing
+    def showTodoFiles():
 
-    for i in range(len(files)):
+        fileDisplayTextArea.config(state = NORMAL) # Change state of Text area to allow write
 
-        fileDisplayTextArea.insert(END, str(i + 1) + ": " + files[i] + "\n")
+        fileDisplayTextArea.delete(1.0, END)
+
+        for i in range(len(FILES)):
+
+            # This is probably around O(n^2) but n should be low, refactor if a much larger data set is used  
+            fileDisplayTextArea.insert(END, str(i + 1) + ": " + displayStrippedPath(FILES[i]) + "\n")
+
+            fileDisplayTextArea.insert(END, "")
         
-    fileDisplayTextArea.config(state = DISABLED) # Make read-only again
+        fileDisplayTextArea.config(state = DISABLED) # Make read-only again
 
-# Checks if input from spinner is a valid file
-def spinnerInputValidator(input):
+    # Checks if input from spinner is a valid file
+    def spinnerInputValidator(input):
 
-    if input.isdigit():
-        if int(input) not in range(1, len(files) + 1):
-            messagebox.showerror("Invalid Range Error","Bad Input: Invalid range, select a listed file number")
-            return FALSE
-    else:
-        messagebox.showerror("Non-digit Error","Bad Input: Not a digit") 
-        return FALSE
-    return TRUE
+        if input.isdigit():
+            if int(input) not in range(1, len(FILES) + 1):
+                messagebox.showerror("Invalid Range Error","Bad Input: Invalid range, select a listed file number")
+                return False
+        else:
+            messagebox.showerror("Non-digit Error","Bad Input: Not a digit") 
+            return False
+        return True
 
-# Displays contents of file selected
-def showFileContents():
-    input = fileSelecter.get()
+    # Used to display a file name in a more readable manner, returns string
+    def displayStrippedPath(pathed):
 
-    if spinnerInputValidator(input):
+        formatted = pathed[pathed.index("\\") + 1:]
 
-        fileContentsTextArea.config(state = NORMAL) # Change state of Text area to allow write
+        return formatted
 
-        fileContentsTextArea.delete(1.0, END)
-        index = int(input) - 1
+    # Displays contents of file selected to default read area
+    def showFileContents():
+        input = fileSelecter.get()
 
-        fOpened = open(files[index], "r")
+        if spinnerInputValidator(input):
 
-        for line in fOpened:
-            fileContentsTextArea.insert(END, line + "\n")
+            fileContentsTextArea.config(state = NORMAL) # Change state of Text area to allow write
 
-        fOpened.close()
-        fileContentsTextArea.config(state = DISABLED) # Make read-only again
+            fileContentsTextArea.delete(1.0, END)
+            index = int(input) - 1
+
+            fOpened = open(FILES[index], "r")
+
+            for line in fOpened:
+                fileContentsTextArea.insert(END, line + "\n")
+
+            fOpened.close()
+            fileContentsTextArea.config(state = DISABLED) # Make read-only again
+
+    # Displays contents of file to a selected read/write area
+    # Arg: text area 
+    def showFileContentsRW(textArea): 
+        input = fileSelecter.get()
+    
+        if spinnerInputValidator(input):
+            textArea.delete(1.0, END)
+            index = int(input) - 1
+
+            fOpened = open(FILES[index], "r")
+
+            for line in fOpened:
+                textArea.insert(END, line + "\n")
+
+            fOpened.close()
+
+    # Write to file
+    def writeToFile(textArea):
+        confirmWrite = messagebox.askquestion(
+            "Confirm Change", "Are you sure you want to apply these changes to file: "
+                                              + displayStrippedPath(CURRENT_FILE) + " ?")
+
+        if confirmWrite == 'yes':
+            
+            fWrite = open(CURRENT_FILE, "w")
+
+            fWrite.write(textArea.get(1.0, END))
+
+            fWrite.close
+            return True
+        else:
+            return False
+
+    # Open new window to edit file
+    def newEditWindow():
+        input = fileSelecter.get()
+        global WRITE_WINDOWS_OPEN
+
+        if spinnerInputValidator(input) and WRITE_WINDOWS_OPEN == 0:
+
+            # When editWindow close event
+            def onClosing():
+                if messagebox.askokcancel("Exit", "Do you want to exit? Changes will not be saved."):
+                    global WRITE_WINDOWS_OPEN
+                    WRITE_WINDOWS_OPEN -= 1
+                    editWindow.destroy()
+
+            # Used to apply the file write and close the window 
+            def applyingChanges():
+            
+                if writeToFile(editingTextArea):
+                    global WRITE_WINDOWS_OPEN   # Controls the number of editing windows open |default: 1 max
+                    global APPLY_ACTIVE         # Deactivates exit alert if Apply button used in favor of apply alert
+
+                    APPLY_ACTIVE = 0            # 0 using the bool() method results false, 1 results true
+                    WRITE_WINDOWS_OPEN -= 1
+
+                    editWindow.destroy()
+
+            global CURRENT_FILE
+            CURRENT_FILE = FILES[int(input) - 1]
+            editWindow = Toplevel()
+            WRITE_WINDOWS_OPEN += 1
+            APPLY_ACTIVE = 1
+
+            # Sets title and size
+            editWindow.title("Schedule Editing Window")
+            editWindow.geometry("1400x600")
+
+            # scrollbar setup
+            textScroll = Scrollbar(editWindow, orient=VERTICAL)
+            textScroll.pack(side = RIGHT, fill = Y)
+
+            editingTextArea = Text(editWindow,
+                                   width = 150,
+                                   height = 30,
+                                   yscrollcommand = textScroll.set)
+
+            editingTextArea.pack(side = RIGHT, fill = Y)
+
+            applyEditButton = Button(editWindow,
+                                     text = "Apply Changes",
+                                     command = applyingChanges) # Make command for apply changes
+            applyEditButton.pack(side = LEFT)
+
+            # scrollbar config
+            textScroll.config(command=editingTextArea.yview)
+            showFileContentsRW(editingTextArea)
+
+            # Controls exit state of the window
+            if(bool(APPLY_ACTIVE)):
+                editWindow.protocol("WM_DELETE_WINDOW", onClosing)
 
 
-# Open new window to edit file
-def newEditWindow():
-    input = fileSelecter.get()
+    # Main Window
+    mainWindow = Tk()
 
-    if spinnerInputValidator(input):
-        editWindow = Toplevel()
+    # mainWindow size
+    mainWindow.geometry('500x500')
 
-        # Sets title and size
-        editWindow.title("Schedule Editing Window")
-        editWindow.geometry("1400x600")
+    # background color
+    mainWindow.config(background = "white")
 
-        #scrollbar setup
-        textScroll = Scrollbar(editWindow, orient=VERTICAL)
-        textScroll.pack(side = RIGHT, fill = Y)
+    # titling mainWindow
+    mainWindow.title("Time Manager")
 
-        editingTextArea = Text(editWindow,
-                               width = 150,
-                               height = 30,
-                               yscrollcommand = textScroll.set)
+    selectFrame = Frame(mainWindow)
 
-        editingTextArea.pack(side = RIGHT, fill = Y)
+    # Button: initiate command to find files
+    showFilesButton = Button(mainWindow,
+                             text = "Show Files",
+                             command = showTodoFiles)
 
-        applyEditButton = Button(editWindow,
-                                 text = "Apply Changes") # Make command for apply changes
-        applyEditButton.pack(side = LEFT)
+    # TextArea: display files here
+    fileDisplayTextArea = Text(mainWindow,
+                             width = 50, height = 10,
+                             state = DISABLED,
+                             fg = "black",
+                             bg = "lightBlue")
 
-        #scrollbar config
-        textScroll.config(command=editingTextArea.yview)
+    # Button: Read file Selected
+    ShowFile = Button(selectFrame,
+                             text = "Show File",
+                             command = showFileContents)
 
+    # Button: Edit file selected
+    editFileButton = Button(selectFrame,
+                              text = "Edit File",
+                              command = newEditWindow )
 
+    # SpinBox: Select file
 
-# Main Window
-mainWindow = Tk()
+    fileSelecter = Spinbox(selectFrame,
+                           from_=1, to = len(FILES) )
 
-# mainWindow size
-mainWindow.geometry('500x500')
+    # TextArea: display file contents here
+    fileContentsTextArea = Text(mainWindow,
+                             width = 50, height = 10,
+                             state = DISABLED,
+                             fg = "black",
+                             bg = "lightBlue")
 
-# background color
-mainWindow.config(background = "white")
+    # Assign all widgets to mainWindow grid
+    showFilesButton.grid(column = 1, row = 1)
 
-# titling mainWindow
-mainWindow.title("Time Manager")
+    fileDisplayTextArea.grid(column = 1, row = 2)
 
-selectFrame = Frame(mainWindow)
+    selectFrame.grid(column = 1, row = 3) # Holds both widgets below that use .pack
 
-# Button: initiate command to find files
-showFilesButton = Button(mainWindow,
-                         text = "Show Files",
-                         command = showTodoFiles)
+    ShowFile.pack( side = LEFT) # stacks/packs starting left, allows them to be side by side 
 
-# TextArea: display files here
-fileDisplayTextArea = Text(mainWindow,
-                         width = 50, height = 10,
-                         state = DISABLED,
-                         fg = "black",
-                         bg = "lightBlue")
+    fileSelecter.pack( side = LEFT)
 
-# Button: Read file Selected
-ShowFile = Button(selectFrame,
-                         text = "Show File",
-                         command = showFileContents)
+    editFileButton.pack( side = LEFT )
 
-# Button: Edit file selected
-editFileButton = Button(selectFrame,
-                          text = "Edit File",
-                          command = newEditWindow ) # Add command functionality
-
-# SpinBox: Select file
-fileSelecter = Spinbox(selectFrame,
-                       from_=1, to = len(files) )
-
-# TextArea: display file contents here
-fileContentsTextArea = Text(mainWindow,
-                         width = 50, height = 10,
-                         state = DISABLED,
-                         fg = "black",
-                         bg = "lightBlue")
-
-# Assign all widgets to mainWindow grid
-showFilesButton.grid(column = 1, row = 1)
-
-fileDisplayTextArea.grid(column = 1, row = 2)
-
-selectFrame.grid(column = 1, row = 3) # Holds both widgets below that use .pack
-
-ShowFile.pack( side = LEFT) # stacks/packs starting left, allows them to be side by side 
-
-fileSelecter.pack( side = LEFT)
-
-editFileButton.pack( side = LEFT )
-
-fileContentsTextArea.grid(column = 1, row = 4)
+    fileContentsTextArea.grid(column = 1, row = 4)
 
 
 
 
 
-mainWindow.mainloop()
+    mainWindow.mainloop()
